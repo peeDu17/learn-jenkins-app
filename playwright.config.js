ea3b33c2e848acbc8ff66f7e11d44cdc8b069561
Sudeep Regmi <peedus17@gmail.com>
2025-05-28 19:16:37 +0000

    Test

--- playwright.config.js
+++ playwright.config.js
@@ -22,9 +22,9 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html'],
-    ['junit']
-  ],
+  ['html'],
+  ['junit', { outputFile: 'test-results/results.xml' }]
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
